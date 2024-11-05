693b7239c7e1e0e4559dc3fc7cda1254ba474b91
swaroop <swaroopiit@gmail.com>
2024-11-05 20:30:49 +0530

    Solved some more problems

--- two_pointes/p01_token_replace/p01_token_replace.py
+++ two_pointes/p01_token_replace/p01_token_replace.py
@@ -4,7 +4,7 @@ def token_replace(s, tokens):
     j = 1
     while i < len(s):
         if s[i] != '$':
-            output.append(s[i])
+            output += s[i]
             i += 1
             j = i + 1
         elif s[j] != '$':
