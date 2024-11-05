7ad410fdfd86ef103f5fbfd909a3704bff496aaa
swaroop <swaroopiit@gmail.com>
2024-11-05 20:25:31 +0530

    Solved some more problems

--- two_pointes/p01_token_replace/p01_token_replace.py
+++ two_pointes/p01_token_replace/p01_token_replace.py
@@ -1,21 +1,21 @@
 def token_replace(s, tokens):
-  output = []
-  i = 0
-  j = 1
-  while i < len(s):
-    if s[i] != '$':
-      output.append(s[i])
-      i += 1
-      j = i + 1
-    elif s[j] != '$':
-      j += 1
-    else:
-      key = s[i: j + 1]
-      output.append(tokens[key])
-      i = j + 1
-      j = i + 1
+    output = []
+    i = 0
+    j = 1
+    while i < len(s):
+        if s[i] != '$':
+            output.append(s[i])
+            i += 1
+            j = i + 1
+        elif s[j] != '$':
+            j += 1
+        else:
+            key = s[i: j + 1]
+            output.append(tokens[key])
+            i = j + 1
+            j = i + 1
   
-  return ''.join(output)
+    return ''.join(output)
 
         
 tokens = {
@@ -23,4 +23,4 @@ tokens = {
   '$ANIMAL$': 'dog',
 }
 print(token_replace('Walk the $ANIMAL$ in the $LOCATION$!', tokens))
-# -> 'Walk the dog in the park!' 
+# -> 'Walk the dog in the park!'
